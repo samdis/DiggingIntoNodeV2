bfb0a0db8904b2f82c948302900a2971f2f67f7a
Sam D <38329857+samdis@users.noreply.github.com>
2021-12-24 16:00:45 -0700

    Checking in after ex7

--- exercises/ex7.js
+++ exercises/ex7.js
@@ -3,13 +3,14 @@
 "use strict";
 
 var util = require("util");
-// var childProc = require("child_process");
+var childProc = require("child_process");
+
 
 
 // ************************************
 
 const HTTP_PORT = 8039;
-// const MAX_CHILDREN = 5;
+const MAX_CHILDREN = 1500;
 
 var delay = util.promisify(setTimeout);
 
@@ -20,5 +21,43 @@ main().catch(console.error);
 // ************************************
 
 async function main() {
-	// console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+	console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+
+
+	while(true) {
+		process.stdout.write(`Trying ${MAX_CHILDREN} requests...`);
+
+		let children = [];
+
+		for(let i=0; i < MAX_CHILDREN; i++) {
+			let child = childProc.spawn("node", [ "ex7-child.js" ]);
+			children.push(child);
+		}
+
+		let resps = children.map(function wait(child){
+			return new Promise(function c(res){
+				child.on("exit",function onExit(code){
+					if (code === 0) res(true);
+					res(false);
+				});
+			});
+		});
+
+		resps = await Promise.all(resps);
+
+		if (resps.every(Boolean)) {
+			console.log(" success!");
+		}
+		else {
+			console.log(" failures.");
+		}
+
+		await delay(500);
+	}
+
+
+	var child = childProc.spawn("node", [ "ex7-child.js" ]);
+	child.on("exit", function(code){
+		console.log("Child finished", code);
+	});
 }
